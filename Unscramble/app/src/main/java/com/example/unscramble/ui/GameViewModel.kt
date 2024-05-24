09b587076fec8b8906144e5761771a9027b60c51
OutisMed <medardkik01@gmail.com>
2024-05-24 11:29:35 +0200

    corriger l'erreur dans la classe GameView

--- Unscramble/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ Unscramble/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -3,18 +3,16 @@ package com.example.unscramble.ui
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class GameViewModel:ViewModel() {
     // Game UI state
     private val _uiState = MutableStateFlow(GameUiState())
-    val uiState: StateFlow<GameUiState>
-        get() {
-            TODO()
-        }
+    val uiState: StateFlow<GameUiState > = _uiState.asStateFlow()
     private var _count = 0
 
     val count: Int
         get() = _count
-    
+
 
 }
