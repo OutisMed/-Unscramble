666bf6ae10fbad15a5a777c2330e4d73b112f4a2
OutisMed <medardkik01@gmail.com>
2024-05-24 11:24:53 +0200

    mise à jour dans la classe gameViewModel

--- Unscramble/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ Unscramble/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -2,8 +2,19 @@ package com.example.unscramble.ui
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class GameViewModel:ViewModel() {
     // Game UI state
     private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState>
+        get() {
+            TODO()
+        }
+    private var _count = 0
+
+    val count: Int
+        get() = _count
+    
+
 }
